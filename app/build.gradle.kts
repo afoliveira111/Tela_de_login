e6dfaef74e51058754385c74cb461a866b116609
Felipe <felipetermometria@hotmail.com>
2023-11-29 17:50:53 -0300

    Criando XML  Update Login Password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.tela_de_login"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tela_de_login"
