03360f066b8dca4d77eb36a9d142deb7ed01bac5
Felipe <felipetermometria@hotmail.com>
2023-11-29 18:00:56 -0300

    if (username.equals("user") && password.equals("pass")){                 Toast.makeText(applicationContext, "Login OK", Toast.LENGTH_SHORT).show()             }else{                 Toast.makeText(applicationContext, "Login Inválido", Toast.LENGTH_SHORT).show()             }
                binding.editUsername.setText("")
                binding.editPassword.setText("")

--- app/src/main/java/com/example/tela_de_login/MainActivity.kt
+++ app/src/main/java/com/example/tela_de_login/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.tela_de_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tela_de_login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonEntrar.setOnClickListener {
+            val username = binding.editUsername.text.toString().trim()
+            val password = binding.editPassword.text.toString().trim()
+
+            if (username.equals("user") && password.equals("pass")){
+                Toast.makeText(applicationContext, "Login OK", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "Login Inválido", Toast.LENGTH_SHORT).show()
+            }
+
+            binding.editUsername.setText("")
+            binding.editPassword.setText("")
+        }
+
     }
 }
