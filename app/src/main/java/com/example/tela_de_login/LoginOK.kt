54811de90467eaf63761d3a1073feeceeef2b43c
Felipe <felipetermometria@hotmail.com>
2023-11-30 16:28:25 -0300

    binding config

--- app/src/main/java/com/example/tela_de_login/LoginOK.kt
+++ app/src/main/java/com/example/tela_de_login/LoginOK.kt
@@ -2,10 +2,20 @@ package com.example.tela_de_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tela_de_login.databinding.ActivityLoginOkBinding
 
 class LoginOK : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginOkBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_ok)
+        binding = ActivityLoginOkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonLogout.setOnClickListener {
+            finish()
+        }
     }
 }
