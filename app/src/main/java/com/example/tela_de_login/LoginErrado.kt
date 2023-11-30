66804609de0e2a4f18672ed532ba2b1835d1b0cd
Felipe <felipetermometria@hotmail.com>
2023-11-30 16:30:52 -0300

    Activity login errado

--- app/src/main/java/com/example/tela_de_login/LoginErrado.kt
+++ app/src/main/java/com/example/tela_de_login/LoginErrado.kt
@@ -2,10 +2,19 @@ package com.example.tela_de_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tela_de_login.databinding.ActivityLoginErradoBinding
 
 class LoginErrado : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginErradoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_errado)
+        binding = ActivityLoginErradoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonVoltar.setOnClickListener {
+          finish()
+        }
     }
 }
